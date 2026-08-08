0eb3f5d526a9cc1fb76b781bb5b769694a81484f
Digvijay Thorat <digvijaythorat855@gmail.com>
2026-08-08 12:03:30 +0000

    Java-Gradle-Project

---
+++ src/main/java/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java Gradle Application!");
+        System.out.println("Task 3 - Gradle Build Successful!");
+    }
+}
